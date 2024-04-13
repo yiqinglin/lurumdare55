90008691b8a9aa146bfbfcdfa8f3480d6d7e6f1b
Claire Lin <tinyfinn@claires-air.lan>
2024-04-13 13:11:53 -0700

    misc: Moved scenes to Scenes folder and added assets.

--- Assets/Scripts/MealManager.cs
+++ Assets/Scripts/MealManager.cs
@@ -10,55 +10,83 @@ public class MealManager : MonoBehaviour
     private string food2 = "";
     private string cat1 = "";
     private string cat2 = "";
-    void Start() {
+    void Start()
+    {
         Play();
     }
-    void Play() {
+    void Play()
+    {
         food1 = "";
         food2 = "";
     }
-    public bool InCreaseFood(string food) {
+    public bool InCreaseFood(string food)
+    {
         Debug.Log(" 返回的什么" + food);
-        if (food1 == "") {
+        if (food1 == "")
+        {
             food1 = food;
-        } else if (food2 == "") {
+        }
+        else if (food2 == "")
+        {
             food2 = food;
-        } else {
+        }
+        else
+        {
             return false;
         }
         return true;
     }
     // 计算来的是什么猫猫
-    public void Summoning() {
-        if (food1 == "Food 1(Clone)") {
+    public void Summoning()
+    {
+        if (food1 == "Food 1(Clone)")
+        {
             cat1 = "CatA";
-            if (food2 == "Food 2(Clone)") {
+            if (food2 == "Food 2(Clone)")
+            {
                 cat2 = "CatB";
-            } else if (food2 == "Food 3(Clone)") {
+            }
+            else if (food2 == "Food 3(Clone)")
+            {
                 cat2 = "CatE";
-            } else if (food2 == "Food 3(Clone)") {
+            }
+            else if (food2 == "Food 3(Clone)")
+            {
                 cat2 = "CatD";
             }
-        } else if (food1 == "Food 2(Clone)") {
-            if (food2 == "Food 1(Clone)") {
+        }
+        else if (food1 == "Food 2(Clone)")
+        {
+            if (food2 == "Food 1(Clone)")
+            {
                 cat1 = "CatA";
                 cat2 = "CatB";
-            } else if (food2 == "Food 3(Clone)") {
+            }
+            else if (food2 == "Food 3(Clone)")
+            {
                 cat1 = "CatB";
                 cat2 = "CatC";
-                
+
             }
-        } else if (food1 == "Food 3(Clone)") {
-            if (food2 == "Food 1(Clone)") {
+        }
+        else if (food1 == "Food 3(Clone)")
+        {
+            if (food2 == "Food 1(Clone)")
+            {
                 cat1 = "CatA";
                 cat2 = "CatE";
-            } else if (food2 == "Food 2(Clone)") {
+            }
+            else if (food2 == "Food 2(Clone)")
+            {
                 cat1 = "CatB";
                 cat2 = "CatC";
-                
+
             }
-        } else if (food1 == "Food 4(Clone)") {
-            if (food2 == "Food 1(Clone)") {
+        }
+        else if (food1 == "Food 4(Clone)")
+        {
+            if (food2 == "Food 1(Clone)")
+            {
                 cat1 = "CatD";
             }
         }
